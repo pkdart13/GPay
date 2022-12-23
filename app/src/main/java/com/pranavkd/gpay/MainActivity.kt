176afff5db2939f9bab953b8aae85683150440a2
anonymou <gooo@gmail.com>
2022-12-23 14:39:35 +0000

    initial

--- app/src/main/java/com/pranavkd/gpay/MainActivity.kt
+++ app/src/main/java/com/pranavkd/gpay/MainActivity.kt
@@ -1,11 +1,2 @@
 package com.pranavkd.gpay
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
