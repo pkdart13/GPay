6652de9dc8b86f687e907bf676e6add108f77c60
pranavkd <pkdart15@gmail.com>
2022-12-24 04:50:00 +0000

    initially

--- app/src/main/java/com/pranavkd/gpay/Home.kt
+++ app/src/main/java/com/pranavkd/gpay/Home.kt
@@ -1,11 +1,18 @@
 package com.pranavkd.gpay
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 class Home : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
